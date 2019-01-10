26653b7003a1b129d7d4e8778009e8efc9bcfd78
nayyarawan <nayyar.awan@gmail.com>
2019-01-10 17:29:54 +0100

    last update

--- ex5/src/robotquest-web.js
+++ ex5/src/robotquest-web.js
@@ -7,6 +7,7 @@ let maxLineIndex = 3;
 let maxColumnIndex = 3;
 let nbOfMoves = 0;
 let headArrow = "↑";
+let newBoard = "";
 function main() {
     let robot ={
         position:{
@@ -15,12 +16,12 @@ function main() {
         },
         head : "up"
     }
-    console.log("Starting now!");
-
+    newBoard = document.getElementById("board-inner").innerHTML;
     // attach events to your elements/buttons
     document.getElementById("turn-left-button").addEventListener("click", function(){turn_left(robot);});
     document.getElementById("turn-right-button").addEventListener("click", function(){turn_right(robot);});
     document.getElementById("move-button").addEventListener("click", function(){ move_robot(robot); });
+    document.getElementById("restart-game").addEventListener("click", function(){restart_game(robot)});
     // Initialize your global variables (robot, nbOfMoves, etc...)
 
 
@@ -28,26 +29,117 @@ function main() {
 
 function move_robot(robot) {
 
-    console.log(robot);
+    let lastRobot = JSON.parse(JSON.stringify(robot));
+    //console.log(lastRobot);
     // call the move function from robotquest-features (already imported 4U)
-    move(robot, maxLineIndex, maxColumnIndex, nbOfMoves);
-    document.getElementById("l"+robot.position.line+"c"+robot.position.column).innerHTML="R"+headArrow;
+    nbOfMoves = move(robot, maxLineIndex, maxColumnIndex, nbOfMoves);
+    let nextCell = document.getElementById("l"+robot.position.line+"c"+robot.position.column).innerText;
+    //console.log(robot);
+        document.getElementById("l" + robot.position.line + "c" + robot.position.column).innerHTML = "R" + headArrow;
+        document.getElementById("output1").innerHTML = "Moves : " + nbOfMoves;
 
+        switch (nextCell.trim()) {
+            case "F" :
+                document.getElementById("result").innerHTML = "Robot have reached flag! Successfully";
+                document.getElementById("result").style.display = "inline-block";
+                document.getElementById("restart-game").style.display = "inline-block";
+                document.getElementById("restart-game").innerHTML = "New Game";
+                disable_btn();
+                break;
+            case "W":
+                document.getElementById("result").innerHTML = "Oop! Robot have reached to water";
+                document.getElementById("result").style.display = "inline-block";
+                document.getElementById("restart-game").style.display = "inline-block";
+                document.getElementById("restart-game").innerHTML = "Restart Game";
+                disable_btn();
+                break;
+            case "T":
+
+                document.getElementById("result").style.display = "inline-block";
+                document.getElementById("result").style.color = "yellow";
+                document.getElementById("result").innerHTML = "Robot have reached Tree";
+                document.getElementById("l"+robot.position.line+"c"+robot.position.column).classList.add("shake");
+
+                setTimeout(function () {
+                    document.getElementById("l"+robot.position.line+"c"+robot.position.column).innerHTML = nextCell;
+                    document.getElementById("l"+robot.position.line+"c"+robot.position.column).classList.remove("shake");
+                    removeStep(robot);
+                    document.getElementById("result").style.display = "none";
+                    document.getElementById("result").style.color = "White";
+
+                }, 1500);
+
+
+                //console.log(robot);
+                break;
+
+        }
+
+}
+
+function removeStep(robot){
+    switch (robot.head) {
+        case "up":
+            robot.position.line -=1;
+            break;
+        case "down":
+            robot.position.line +=1;
+            break;
+        case "left":
+            robot.position.column +=1;
+            break;
+        case "right":
+            robot.position.column -=1;
+            break;
+    }
+    nbOfMoves -=1;
+    document.getElementById("output1").innerHTML = "Moves : " + nbOfMoves;
 }
 
 function turn_left(robot) {
     console.log("Turn left");
     // you can call this function directly from robotquest-features.js (see import at top of file):
-    turn(robot, 'turn-left', turns);
+    turns = turn(robot, 'turn-left', turns);
     turn_head(robot);
+    document.getElementById("output2").innerHTML = "Turns : "+ turns;
 }
 
 
 function turn_right(robot) {
     console.log("Turn right");
     // you can call this function directly from robotquest-features.js (see import at top of file):
-    turn(robot, 'turn-right', turns);
+     turns = turn(robot, 'turn-right', turns);
     turn_head(robot);
+    document.getElementById("output2").innerHTML = "Turns : "+ turns;
+}
+
+function restart_game(robot){
+    document.getElementById("board-inner").innerHTML = newBoard;
+    document.getElementById("result").style.display = "none";
+    document.getElementById("restart-game").style.display = "none";
+
+    headArrow = "↑";
+    robot.head="up";
+    robot.position.column = 0;
+    robot.position.line = 0;
+    nbOfMoves = 0;
+    turns = 0;
+    enable_btn();
+
+    document.getElementById("output1").innerHTML = "Moves : " + nbOfMoves;
+    document.getElementById("output2").innerHTML = "Turns : " + turns;
+}
+
+function disable_btn(){
+    document.getElementById("turn-left-button").disabled = true;
+    document.getElementById("turn-right-button").disabled = true;
+    document.getElementById("move-button").disabled = true;
+}
+
+function enable_btn(){
+    document.getElementById("turn-left-button").disabled = false;
+    document.getElementById("turn-right-button").disabled = false;
+    document.getElementById("move-button").disabled = false;
 }
 
 function turn_head(robot){
